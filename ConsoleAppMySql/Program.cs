b3b3f1a22c6c19d52c9c9f3cf37d8e2f4a0697ca
jimiwang <jimiwang@micron.com>
2018-03-06 19:38:21 +0800

    add files

---
+++ ConsoleAppMySql/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "sakila";
+            if (dbCon.IsConnect())
+            {
+                //suppose col0 and col1 are defined as VARCHAR in the DB
+                string query = "select * from customer limit 20";
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string someStringFromColumnZero = reader.GetString(2);
+                    string someStringFromColumnOne = reader.GetString(1);
+                    Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                }
+                dbCon.Close();
+            }
+        }
+    }
+}
